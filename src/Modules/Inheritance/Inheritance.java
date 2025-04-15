9ca4fa5d7151b0e04862fd16cf435a141635dc89
Jayasurya <jayasurya@atatus.com>
2025-04-15 22:05:22 +0530

    feat: fix name correction

--- src/Modules/Inheritance/Inheritance.java
+++ src/Modules/Inheritance/Inheritance.java
@@ -5,13 +5,16 @@ import java.util.Date;
 
 public class Inheritance {
     public static void main(String[] args) {
+        Manager mgr = new Manager("Surya", 30, 100000.0, "Software Engineer", new Date(), 10000.0);
         ArrayList<Employee> empList = new ArrayList<>();
 
         // Adding 20 Employees
         for (int i = 1; i <= 20; i++) {
-            String name = NameGenerator.getRandonName();
+            String name = NameGenerator.getRandomName(); // Fixed typo here
             empList.add(new Employee(name, 20 + i, 50000.0 + (i * 1000), "Department" + i, new Date()));
         }
+
+        empList.add(mgr); // Fixed the issue with adding Manager
 
         // Display each employee's details
         for (Employee e : empList) {
--- src/Modules/Inheritance/NameGenerator.java
+++ src/Modules/Inheritance/NameGenerator.java
@@ -11,7 +11,7 @@ public class NameGenerator {
             "Kumar", "Sharma", "Reddy", "Verma", "Nair", "Das", "Patel", "Joshi", "Menon", "Chopra"
     };
 
-    public static String getRandonName() {
+    public static String getRandomName() {
         Random rand = new Random();
         String fist = firstNames[rand.nextInt(firstNames.length)];
 //        return firstName()[(int) (Math.random() * firstName().length)] + " " + lastName()[(int) (Math.random() * lastName().length)];
